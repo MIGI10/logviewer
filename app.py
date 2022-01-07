83066ef13f87b56ae3de3e50b408f0d6e6d3ccb6
MIGI10 <miguelpalau1@gmail.com>
2022-01-07 05:20:44 +0100

    add discord oauth to check roles in a guild and log access clearance

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 __version__ = "1.1"
 
 import os
+import requests
+
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from sanic import Sanic, response
@@ -31,6 +33,56 @@ def render_template(name, *args, **kwargs):
     return response.html(template.render(*args, **kwargs))
 
 
+async def oauth_check(request, document, key):
+
+    redirect_uri = os.getenv("OAUTH_URI") + "&state=" + key
+
+    if request.query_args:
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        data = {
+            'client_id': os.getenv("OAUTH_CLIENT_ID"),
+            'client_secret': os.getenv("OAUTH_CLIENT_SECRET"),
+            'grant_type': 'authorization_code',
+            'code': request.query_args[0][1],
+            'redirect_uri': os.getenv("OAUTH_REDIRECT_URI")
+        }
+
+        oauth = requests.post('https://discord.com/api/v9/oauth2/token', data=data, headers=headers)
+        oauth = oauth.json()
+
+        if "error" in oauth: 
+            return redirect_uri
+
+        headers = {
+            'authorization': oauth["token_type"] + ' ' + oauth["access_token"]
+        }
+
+        user = requests.get('https://discord.com/api/v9/users/@me', headers=headers)
+        user = user.json()
+
+        print("{}#{} with ID {} accessing site".format(user["username"], user["discriminator"], user["id"]))
+
+        member = requests.get('https://discord.com/api/v9/users/@me/guilds/739552045123764275/member', headers=headers)
+        member = member.json()
+        
+        if '822253899700371488' not in member["roles"]:
+            if document["title"] == "minecraft" and '771566558899994645' in member["roles"]:
+                pass
+            else:
+                return "403"
+
+        if '739552527288107109' not in member["roles"] and document["title"] == "admin":
+            return "403"
+
+        return None
+
+    else:
+        return redirect_uri
+
 app.ctx.render_template = render_template
 
 db_name = os.getenv("MONGO_DB_NAME")
@@ -49,13 +101,32 @@ async def index(request):
     return render_template("index")
 
 
+@app.get("/return")
+async def redirect_to_log(request):
+
+    if not request.query_args:
+        return render_template("index")
+    
+    return response.redirect('http://' + request.host + prefix + '/' + request.query_args[1][1] + '?code=' + request.query_args[0][1])
+
+
 @app.get(prefix + "/raw/<key>")
 async def get_raw_logs_file(request, key):
-    """Returns the plain text rendered log entry"""
+
     document = await app.ctx.db.logs.find_one({"key": key})
 
     if document is None:
-        abort(404)
+        return abort(404)
+
+    action = await oauth_check(request, document, key)
+
+    if action is not None:
+        if action == "404":
+            abort(404)
+        elif action == "403":
+            abort(403)
+        else:
+            response.redirect(action)
 
     log_entry = LogEntry(app, document)
 
@@ -64,11 +135,21 @@ async def get_raw_logs_file(request, key):
 
 @app.get(prefix + "/<key>")
 async def get_logs_file(request, key):
-    """Returns the html rendered log entry"""
+
     document = await app.ctx.db.logs.find_one({"key": key})
 
     if document is None:
-        abort(404)
+        return abort(404)
+
+    action = await oauth_check(request, document, key)
+
+    if action is not None:
+        if action == "404":
+            return abort(404)
+        elif action == "403":
+            return abort(403)
+        else:
+            return response.redirect(action)
 
     log_entry = LogEntry(app, document)
 
